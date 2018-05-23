fb0717f7973239039835dd67d4218e5eac3164b7
Timothy Nordloh <tnordloh@gmail.com>
2018-05-23 17:06:33 -0600

    add sqlite3 to sinatra

--- app.rb
+++ app.rb
@@ -8,10 +8,13 @@ $: << File.expand_path('../lib', __FILE__)
 
 # Require base
 require 'sinatra/base'
+require 'sinatra/activerecord'
 require 'active_support/core_ext/string'
 require 'active_support/core_ext/array'
 require 'active_support/core_ext/hash'
 require 'active_support/json'
+
+# Add database
 
 libraries = Dir[File.expand_path('../lib/**/*.rb', __FILE__)]
 libraries.each do |path_name|
@@ -28,6 +31,8 @@ module Blog
     configure do
       set :root, Dir.pwd
       set :views, 'app/views'
+
+      set :database, "sqlite3:myblogdb.sqlite3"
 
       disable :method_override
       disable :static
